9482f398df273fc226c3ccbddad5be0b96a6b317
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2020-05-04 22:31:32 +0200

    Videos 1.1 - 1.5 done

--- C8_In4Hours/Program.cs
+++ C8_In4Hours/Program.cs
@@ -7,7 +7,11 @@ namespace C8_In4Hours
         static void Main(string[] args)
         {
             // Hi there, hey
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            // Build/ Compile, CLR(JIT), MSIL, Machine Language
+            var s1 = new Section1();
+            s1.printHello(); // Hello World
+            s1.readUser();  // Read line, print name
         }
     }
 }
---
+++ C8_In4Hours/Section1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C8_In4Hours
+{
+    class Section1
+    {
+
+
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void ReadUser()
+        {
+            Console.WriteLine("Input name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("name = " + name);
+
+            
+        }
+
+        public void ReadUser2()
+        {
+            // Identifiers = name for class, methods, variables     "System", "C8_In4Hours", "Section1", "ReadUser"
+            // class/ method        = pascal case   "Section1", "PrintHello"
+            // variable/ parameter  = camel case    "myName", "myString"
+
+            // Keywords = related with compiler "using", "namespace", "class", "static", "void", "string"
+            // Statements = action - declaration, expression, selection
+            int i;      // = Declaration statement
+            i = 10;     // = Expression statement
+            if (i > 5)  // = Selection statement
+            {
+                PrintHello();
+            }
+            // Literals = "10" "hi"
+            // Punctuators = "{} , ;"
+            // Operators = + - / * == %
+            // Comments // /**/
+        }
+    }
+}
