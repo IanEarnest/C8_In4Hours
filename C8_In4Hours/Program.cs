17b7406df75044bc110b735ff5a660b8bd70282a
Ian Harcourt-Smith <ianearnest@hotmail.co.uk>
2020-05-05 17:10:53 +0200

    Section 1 end

--- C8_In4Hours/Program.cs
+++ C8_In4Hours/Program.cs
@@ -6,15 +6,30 @@ namespace C8_In4Hours
     {
         static void Main(string[] args)
         {
-            // Hi there, hey
-            //Console.WriteLine("Hello World!");
             // Build/ Compile, CLR(JIT), MSIL, Machine Language
-            var S1 = new Section1();
-            S1.PrintHello(); // Hello World
-            //S1.ReadUser();  // Read line, print name
 
+            // Section 1 = Basic syntax
+            var S1 = new Section1();
+            S1.PrintHello();
+
+            //var S2 = new Section2();
+            //S2.PrintHello(); // Hello World
+        }
+
+        public void S1_Methods()
+        {
+            // S1
+            var S1 = new Section1();
+            S1.PrintHello();
+            S1.ReadUser();
             S1.Lesson1_6();
+            S1.Lesson1_6_2();
             S1.Lesson1_7();
+        }
+        public void S2_Methods()
+        {
+            //var S2 = new Section2();
+            //S2.
         }
     }
 }
--- C8_In4Hours/Section1.cs
+++ C8_In4Hours/Section1.cs
@@ -4,28 +4,47 @@ using System.Text;
 
 namespace C8_In4Hours
 {
+    /// <summary>
+    /// Hello World, read user input, basic syntax, types and variables, arithmetic operators,
+    /// </summary>
     class Section1
     {
-
-
+        /// <summary> Prints "Hello World!"
+        /// </summary>
         public void PrintHello()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("S1 Hello World!");
         }
 
+        /// <summary> Waits for user to input name and then prints it
+        /// <para>  Example:  </para>
+        /// <para>  'Input name: '  </para>
+        /// <para>  -Steve          </para>
+        /// <para>  'name = Steve'  </para>
+        /// </summary>
         public void ReadUser()
         {
             Console.WriteLine("Input name:");
             string name = Console.ReadLine();
             Console.WriteLine("name = " + name);
-
-
         }
 
 
 
 
-
+        /// <summary> Prints "Hello World!"
+        /// <para>    Types and Variables (Contains comments)</para>
+        /// <example> Code:
+        /// <code>
+        ///     int i;
+        ///     i = 10;
+        ///     if (i > 5)
+        ///     {
+        ///         PrintHello();
+        ///     }
+        /// </code>
+        /// </example>
+        /// </summary>
         public void Lesson1_6()
         {
             // Identifiers = name for class, methods, variables     "System", "C8_In4Hours", "Section1", "ReadUser"
@@ -46,13 +65,22 @@ namespace C8_In4Hours
             // Comments     = // /**/
         }
 
+        // Part of Lesson1_6_2
         struct Customer
         {
             public int age;
             public string name;
             public bool isEmployee;
         }
-        public void Lesson1_7()
+        /// <summary> Struct for Customer, printing struct variables, writeline inline condition
+        /// <para></para>
+        ///<example> Code:
+        /// <code>
+        ///     Console.WriteLine("Is he an empoyee? " + (cust.isEmployee ? "Yes" : "No"));
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void Lesson1_6_2()
         {
             // shorthand = cw = console.writeline
             Customer cust;
@@ -64,5 +92,52 @@ namespace C8_In4Hours
             Console.WriteLine("Is he an empoyee? " + (cust.isEmployee ? "Yes" : "No"));
             Console.ForegroundColor = ConsoleColor.White;
         }
+
+        /// <summary> Uses operators +, -, *, /, %, to calculate answer, includes decimal and increment
+        /// <para>    e.g. 15 + 3 = 18</para>
+        /// </summary>
+        public void Lesson1_7()
+        {
+            int firstOperand = 15;
+            int secondOperand = 3;
+
+            //Adding: 15 + 3 = 18
+            int additionResult = firstOperand + secondOperand;
+            int subtractResult = firstOperand - secondOperand;
+            int multiplyResult = firstOperand * secondOperand;
+            int divisionResult = firstOperand / secondOperand;
+            Console.WriteLine("Adding: " + firstOperand + " + " + secondOperand + " = " + additionResult);
+            Console.WriteLine("Subtracting: " + firstOperand + " - " + secondOperand + " = " + subtractResult);
+            Console.WriteLine("Multiplying: " + firstOperand + " * " + secondOperand + " = " + multiplyResult);
+            Console.WriteLine("Dividing: " + firstOperand + " / " + secondOperand + " = " + divisionResult);
+
+            //Adding (decimal): 15 + 3 = answer
+            decimal firstOperandDecimal = 15.0m; // literal to decimal (use M)
+            decimal secondOperandDecimal = 3.0m; // literal to decimal (use M)
+            decimal additionResultDecimal = firstOperandDecimal + secondOperandDecimal;
+            Console.WriteLine("");
+            Console.WriteLine("Adding (decimal): " + firstOperandDecimal + " + " + secondOperandDecimal + " = " + additionResultDecimal);
+
+            // Operator = % (remainder)
+            // firstNum / secondNum = 2 (7/3 = 2)
+            // firstNum % secondNum = 1 (7/3 = 2 with 1 remaining)
+            int firstNum = 7;
+            int secondNum = 3;
+            Console.WriteLine("% operator: " + firstNum + "/" + secondNum + " = " + firstNum/secondNum +  
+                " (remaining = " + firstNum % secondNum + ")");
+            Console.WriteLine("");
+
+            // Increment and decrement "++" "--"
+            int i = 3;
+            int increment = i;
+            Console.WriteLine("i = " + increment);
+            increment = i;
+            increment++;
+            Console.WriteLine("i++ = " + increment);
+            increment = i;
+            increment--;
+            Console.WriteLine("i-- = " + increment);
+        }
+
     }
 }
